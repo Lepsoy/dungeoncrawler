6f02dd1ad6b0965c0cffd941b80e162794e24168
Jonathan Lepsoey <jonna.lepsoy@gmail.com>
2023-11-03 14:06:42 +0100

    (health-and-melee-combat) Create health component and add it to player

--- src/components.rs
+++ src/components.rs
@@ -18,5 +18,11 @@ pub struct MovingRandomly;
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct WantsToMove {
     pub entity: Entity,
-    pub destination: Point
+    pub destination: Point,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Health {
+    pub current: i32,
+    pub max: i32,
 }
