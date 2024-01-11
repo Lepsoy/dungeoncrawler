32bb5bfa7295ab214f1b96a3497a270355196f70
Jonathan Lepsoey <jonna.lepsoy@gmail.com>
2024-01-11 20:54:52 +0100

    (deeper-dungeons) add stairs to the map and store map level in player struct

--- src/map_builder/themes.rs
+++ src/map_builder/themes.rs
@@ -31,7 +31,7 @@ impl MapTheme for ForestTheme {
         match tile_type {
             TileType::Floor => to_cp437(';'),
             TileType::Wall => to_cp437('"'),
-            TileType::Exit => to_cp437('<'),
+            TileType::Exit => to_cp437('>'),
         }
     }
 }
