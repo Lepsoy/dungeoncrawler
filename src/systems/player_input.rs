52a78d5779062f7f7dd682ec2dcdc4956d34232f
Jonathan Lepsoey <jonna.lepsoy@gmail.com>
2024-01-11 23:05:30 +0100

    (combat-systems-and-loot) Prevent carrying multiple swords

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -6,6 +6,7 @@ use crate::prelude::*;
 #[read_component(Item)]
 #[read_component(Player)]
 #[read_component(Point)]
+#[read_component(Weapon)]
 #[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
@@ -33,6 +34,17 @@ pub fn player_input(
                     .for_each(|(entity, _item, _item_pos)| {
                         commands.remove_component::<Point>(*entity);
                         commands.add_component(*entity, Carried(player));
+
+                        if let Ok(e) = ecs.entry_ref(*entity) {
+                            if e.get_component::<Weapon>().is_ok() {
+                                <(Entity, &Carried, &Weapon)>::query()
+                                    .iter(ecs)
+                                    .filter(|(_, c, _)| c.0 == player)
+                                    .for_each(|(e, c, w)| {
+                                        commands.remove(*e);
+                                    })
+                            }
+                        }
                     });
                 Point::new(0, 0)
             }
